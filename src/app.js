55466b9c1336f85ac05b808b9d4dc49bb30dd313
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2026-02-21 02:35:57 +0000

    clase 2, repaso de array-bucles

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ window.onload = function () {
   //////////////////////////////////////////////////////////////////////Array
 
   // index          0         1       2              
-  // length = 3
+  // length =  3
   let alumnos = ["Carlos", "Lucas", "Maria"]
 
 
@@ -122,16 +122,73 @@ while (contador < 10) {
 
 
 
+// ejemplo while
+// contador regresivo:
+
+let numero = 5
+while (numero > 0) {
+  console.log("quedan " + numero + " segundos...");
+  numero = numero - 1 //opcionalmente pueden hacer:  numero--
+ 
+}
+console.log("Despeque");
+
+
+/////////////////////////////////////////////////////while do 
+// do {
+//   //codigo a ejecutar
+// }while (condicion)
+
+// do {
+//   password = prompt("introduce la constraseña")
+// } while (password !== "1234") //el codigo se repite mientras la condicion sea TRUE
+
+// console.log("acceso concedido");
 
 
 
 
 
+////////////////////////////////////////////////////mas ejemplos de for anidado 
+
+  //ejemplo tabla de multiplicar 
+
+  for (let i = 1; i <=2 ; i++) { //bucle de las tablas 
+    for (let j = 1; j <= 10; j++) {  //bucle de los numeros 
+      console.log( i + " x " + j + "=" + (i*j));  //concatenar 
+      
+      
+    }
+    
+  }
+
+// ejemplo crear frases 
+
+let adjetivos = ["Increible", "Fantastico", "Unico"]
+let productos = ["telefono", "reloj", "televisor"]
+
+for (let i = 0; i < adjetivos.length; i++) {
+  let adjetivoActual = adjetivos[i]
+
+  for (let j = 0; j < productos.length; j++) {
+    let productoActual = productos[j]
+
+    let frasePromocional = adjetivoActual + " " + productoActual + " Compra Ahora!!"
+    console.log(frasePromocional);
+    
+  }
+}
 
 
 
 
+///////////////////////MATH RANDOM 
 
+
+let colores = [ "rojo", "verde", "azul", "amarillo"]
+let azar = Math.floor(Math.random()* colores.length)
+
+console.log("El color premiado es: " + colores[azar]);
 
 };
 
